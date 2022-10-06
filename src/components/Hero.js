8c0461227cc0d43d3288da3657ff7703b2d4d330
Noam Henrique <noamhenrique@gmail.com>
2022-10-06 19:26:52 -0300

    Creating Navbar and Hero

---
+++ src/components/Hero.js
@@ -0,0 +1,20 @@
+import { Row, Col, Button, Image } from "react-bootstrap";
+import logo from "../assets/logo__magic-pay--mobile.png";
+import logoDesktop from "../assets/logo__magic-pay.png";
+
+const Hero = () => (
+  <div class="container-fluid bg-light p-5 text-center text-light">
+    <Row className="my-lg-5">
+      <Col lg className="text-lg-end my-lg-5">
+        <Image className="d-lg-none" src={logo} />
+        <Image className="d-none d-lg-inline-block" src={logoDesktop} />
+      </Col>
+      <Col lg className="text-lg-start my-lg-5">
+        <p>Pague suas contas pelo nosso APP</p>
+        <Button variant="outline-light">Abra sua conta</Button>
+      </Col>
+    </Row>
+  </div>
+);
+
+export default Hero;
