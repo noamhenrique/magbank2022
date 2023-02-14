71f09615f20ca8bfc190651558aafbc5d5770df8
Noam Henrique <noamhenrique@gmail.com>
2023-02-14 20:06:35 -0300

    CreditCard section created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import CreditCard from "./components/CreditCard";
 import "./App.scss";
 
 const App = () => (
   <div className="App">
     <Navbar />
     <Hero />
+
+    <CreditCard />
   </div>
 );
 
