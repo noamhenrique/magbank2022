3d88359a96d738a698709c9beccd5668d0d20b7d
Noam Henrique <noamhenrique@gmail.com>
2023-03-02 21:31:02 -0300

    yip feet(footer): Creating the footer

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import IconText from "./IconText";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -11,123 +12,125 @@ import Accordion from "react-bootstrap/Accordion";
 
 import "./Faq.scss";
 
-const Faq = () => (
-  <section className="faq text-light">
-    <Container className="py-5">
-      <Row className="d-flex align-items-center justify-items-center">
-        <Col className="d-lg-none mb-5" xs={12}>
-          <Row>
-            <Col className="d-flex justify-content-center">
-              <FontAwesomeIcon icon={faCreditCard} size="2x" color="#FFF" />
-            </Col>
-            <Col className="d-flex justify-content-center">
-              <FontAwesomeIcon icon={faWallet} size="2x" color="#FFF" />
-            </Col>
-            <Col className="d-flex justify-content-center">
-              <FontAwesomeIcon icon={faShieldAlt} size="2x" color="#FFF" />
-            </Col>
-            <Col className="d-flex justify-content-center">
-              <FontAwesomeIcon icon={faUserTie} size="2x" color="#FFF" />
-            </Col>
-          </Row>
-        </Col>
-        <Col className="d-none d-lg-block">
-          <IconText
-            icon={faCreditCard}
-            size={3}
-            className="mb-3"
-            textClassName="lead"
-            color="#FFF"
-          >
-            Cartão de crédito e débito
-          </IconText>
-          <IconText
-            icon={faWallet}
-            size={3}
-            className="mb-3"
-            textClassName="lead"
-            color="#FFF"
-          >
-            Conta e abertura
-          </IconText>
-          <IconText
-            icon={faShieldAlt}
-            size={3}
-            className="mb-3"
-            textClassName="lead"
-            color="#FFF"
-          >
-            Token digital
-          </IconText>
-          <IconText icon={faUserTie} size={3} textClassName="lead" color="#FFF">
-            Produtos e serviços
-          </IconText>
-        </Col>
-        <Col>
-          <Accordion defaultActiveKey="0">
-            <Accordion.Item eventKey="0">
-              <Accordion.Header>Lorem ipsum dolor sit amet</Accordion.Header>
-              <Accordion.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                tincidunt arcu vel arcu fermentum, eget accumsan dolor
-                dignissim. Nam eget quam semper, varius ligula sed, auctor
-                augue. Maecenas et porta magna. Nulla mattis tortor vitae metus
-                vestibulum scelerisque. Ut mollis a nisl vel suscipit. Curabitur
-                scelerisque placerat ultrices. Proin purus ante, congue vel arcu
-                ac, porta condimentum mauris. Maecenas massa orci, fringilla
-                eget fermentum nec, dignissim nec diam.
-              </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="1">
-              <Accordion.Header>
-                Nulla porttitor eros ac ex tristique posuere
-              </Accordion.Header>
-              <Accordion.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                tincidunt arcu vel arcu fermentum, eget accumsan dolor
-                dignissim. Nam eget quam semper, varius ligula sed, auctor
-                augue. Maecenas et porta magna. Nulla mattis tortor vitae metus
-                vestibulum scelerisque. Ut mollis a nisl vel suscipit. Curabitur
-                scelerisque placerat ultrices. Proin purus ante, congue vel arcu
-                ac, porta condimentum mauris. Maecenas massa orci, fringilla
-                eget fermentum nec, dignissim nec diam.
-              </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="2">
-              <Accordion.Header>
-                Quisque fringilla tincidunt arcu
-              </Accordion.Header>
-              <Accordion.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                tincidunt arcu vel arcu fermentum, eget accumsan dolor
-                dignissim. Nam eget quam semper, varius ligula sed, auctor
-                augue. Maecenas et porta magna. Nulla mattis tortor vitae metus
-                vestibulum scelerisque. Ut mollis a nisl vel suscipit. Curabitur
-                scelerisque placerat ultrices. Proin purus ante, congue vel arcu
-                ac, porta condimentum mauris. Maecenas massa orci, fringilla
-                eget fermentum nec, dignissim nec diam.
-              </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="3">
-              <Accordion.Header>
-                Nam elementum lobortis purus a maximus tortor
-              </Accordion.Header>
-              <Accordion.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                tincidunt arcu vel arcu fermentum, eget accumsan dolor
-                dignissim. Nam eget quam semper, varius ligula sed, auctor
-                augue. Maecenas et porta magna. Nulla mattis tortor vitae metus
-                vestibulum scelerisque. Ut mollis a nisl vel suscipit. Curabitur
-                scelerisque placerat ultrices. Proin purus ante, congue vel arcu
-                ac, porta condimentum mauris. Maecenas massa orci, fringilla
-                eget fermentum nec, dignissim nec diam.
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
-        </Col>
-      </Row>
-    </Container>
-  </section>
-);
+const Faq = () => {
+  const options = [
+    { icon: faCreditCard, text: "Cartão de crédito" },
+    { icon: faWallet, text: "Conta e abertura" },
+    { icon: faShieldAlt, text: "Token digital" },
+    { icon: faUserTie, text: "Produtos e serviços" },
+  ];
+
+  const [index, setIndex] = useState(0);
+
+  const handleClick = (key) => {
+    setIndex(key);
+  };
+
+  return (
+    <section className="faq text-light">
+      <Container className="py-5">
+        <Row className="text-center">
+          <h2 className="faq-title">Dúvidas frequentes</h2>
+        </Row>
+        <Row className="d-flex align-items-center justify-items-center">
+          <Col className="d-lg-none mb-5" xs={12}>
+            <Row>
+              {options.map(({ icon }, key) => (
+                <Col className="d-flex justify-content-center" key={key}>
+                  <FontAwesomeIcon
+                    icon={icon}
+                    size="2x"
+                    color={key === index ? "#FFF" : "#BBB"}
+                    onClick={() => handleClick(key)}
+                  />
+                </Col>
+              ))}
+            </Row>
+            <Row className="text-center mt-5">
+              <p className="lead">{options[index].text}</p>
+            </Row>
+          </Col>
+          <Col className="d-none d-lg-block">
+            {options.map(({ icon, text }, key) => (
+              <IconText
+                icon={icon}
+                size={3}
+                className="faq-clickable mb-3"
+                textClassName="lead"
+                color={key === index ? "#FFF" : "#BBB"}
+                onClick={() => handleClick(key)}
+                key={key}
+              >
+                {text}
+              </IconText>
+            ))}
+          </Col>
+          <Col>
+            <Accordion defaultActiveKey="0" activeKey={`${index}`}>
+              <Accordion.Item eventKey="0">
+                <Accordion.Header>Lorem ipsum dolor sit amet</Accordion.Header>
+                <Accordion.Body>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Nullam tincidunt arcu vel arcu fermentum, eget accumsan dolor
+                  dignissim. Nam eget quam semper, varius ligula sed, auctor
+                  augue. Maecenas et porta magna. Nulla mattis tortor vitae
+                  metus vestibulum scelerisque. Ut mollis a nisl vel suscipit.
+                  Curabitur scelerisque placerat ultrices. Proin purus ante,
+                  congue vel arcu ac, porta condimentum mauris. Maecenas massa
+                  orci, fringilla eget fermentum nec, dignissim nec diam.
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="1">
+                <Accordion.Header>
+                  Nulla porttitor eros ac ex tristique posuere
+                </Accordion.Header>
+                <Accordion.Body>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Nullam tincidunt arcu vel arcu fermentum, eget accumsan dolor
+                  dignissim. Nam eget quam semper, varius ligula sed, auctor
+                  augue. Maecenas et porta magna. Nulla mattis tortor vitae
+                  metus vestibulum scelerisque. Ut mollis a nisl vel suscipit.
+                  Curabitur scelerisque placerat ultrices. Proin purus ante,
+                  congue vel arcu ac, porta condimentum mauris. Maecenas massa
+                  orci, fringilla eget fermentum nec, dignissim nec diam.
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="2">
+                <Accordion.Header>
+                  Quisque fringilla tincidunt arcu
+                </Accordion.Header>
+                <Accordion.Body>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Nullam tincidunt arcu vel arcu fermentum, eget accumsan dolor
+                  dignissim. Nam eget quam semper, varius ligula sed, auctor
+                  augue. Maecenas et porta magna. Nulla mattis tortor vitae
+                  metus vestibulum scelerisque. Ut mollis a nisl vel suscipit.
+                  Curabitur scelerisque placerat ultrices. Proin purus ante,
+                  congue vel arcu ac, porta condimentum mauris. Maecenas massa
+                  orci, fringilla eget fermentum nec, dignissim nec diam.
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="3">
+                <Accordion.Header>
+                  Nam elementum lobortis purus a maximus tortor
+                </Accordion.Header>
+                <Accordion.Body>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Nullam tincidunt arcu vel arcu fermentum, eget accumsan dolor
+                  dignissim. Nam eget quam semper, varius ligula sed, auctor
+                  augue. Maecenas et porta magna. Nulla mattis tortor vitae
+                  metus vestibulum scelerisque. Ut mollis a nisl vel suscipit.
+                  Curabitur scelerisque placerat ultrices. Proin purus ante,
+                  congue vel arcu ac, porta condimentum mauris. Maecenas massa
+                  orci, fringilla eget fermentum nec, dignissim nec diam.
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
 
 export default Faq;
